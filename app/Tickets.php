ef846648b4c5bab0d24999a6e5b718d4e58ce519
rakhadzaky <rakhadzaky9@gmail.com>
2019-10-21 18:54:21 +0700

    make notification that didn't affect anything

--- app/Tickets.php
+++ app/Tickets.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tickets extends Model
 {
     protected $table = 'tickets';
-    protected $primaryKey = 'id_ticket';
+    protected $primaryKey = 'ticket_id';
     protected $fillable = [
         'id_event', 'user_id', 'payment_status'
     ];
+
+    public function user(){
+        return $this->belongsTo('App\User','user_id');
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,5 +88,14 @@
             @yield('content')
         </main>
     </div>
+    <script>
+        if(localStorage.getItem("token")) {
+            let decoded = jwt_decode(localStorage.getItem("token"));
+            window.Echo.private(`App.User.`+decoded.sub)
+                .notification((notification) => {
+                console.log(notification);
+            });
+        }
+    </script>
 </body>
 </html>
